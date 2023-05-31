65eab86f59d5d2a50a9144140b97fe20427f803b
Yerkebulan <134504390+Yerkebulan-Arap@users.noreply.github.com>
2023-05-31 10:17:49 +0600

    Add files via upload

---
+++ work6.py
@@ -0,0 +1,7 @@
+user_input1 = input("Название фильма:")
+user_input2 = input("Название кинотеатра:")
+user_input3 = input("Время:")
+str1 = "Билет на %s" %user_input1
+str2 = " в %s" %user_input2
+str3 = " на %s забронирован" %user_input3
+print(str1+str2+str3)
